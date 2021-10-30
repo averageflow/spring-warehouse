2b627a5fa2a45a51ac3077a85f6f3874f610e815
Josep Jesus Bigorra Algaba <josep@prowarehouse.nl>
2021-10-30 08:51:43 +0200

    init repo

---
+++ src/main/java/nl/averageflow/joeswarehouse/JoesWarehouseApplication.java
@@ -0,0 +1,13 @@
+package nl.averageflow.joeswarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoesWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoesWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/averageflow/joeswarehouse/JoesWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.averageflow.joeswarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoesWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
