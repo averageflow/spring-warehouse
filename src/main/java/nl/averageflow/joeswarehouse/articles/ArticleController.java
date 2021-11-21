72e4e40570879d1affc399070ec74b22c009fda8
averageflow <jjbigorra@gmail.com>
2021-11-21 01:10:31 +0100

    added basic routes

--- src/main/java/nl/averageflow/joeswarehouse/articles/ArticleController.java
+++ src/main/java/nl/averageflow/joeswarehouse/articles/ArticleController.java
@@ -3,12 +3,14 @@ package nl.averageflow.joeswarehouse.articles;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class ArticleController {
 
     @Autowired
--- src/main/java/nl/averageflow/joeswarehouse/transactions/TransactionController.java
+++ src/main/java/nl/averageflow/joeswarehouse/transactions/TransactionController.java
@@ -3,9 +3,13 @@ package nl.averageflow.joeswarehouse.transactions;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@CrossOrigin
 public class TransactionController {
     @Autowired
     private TransactionService transactionService;
