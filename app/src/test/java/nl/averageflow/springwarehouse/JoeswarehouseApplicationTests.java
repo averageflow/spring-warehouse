bdf603d9679f778a71f60e5717a93d47a0933625
averageflow <jjbigorra@gmail.com>
2021-12-09 15:13:44 +0100

    remove initial tests

--- app/src/test/java/nl/averageflow/springwarehouse/JoeswarehouseApplicationTests.java
+++ app/src/test/java/nl/averageflow/springwarehouse/JoeswarehouseApplicationTests.java
@@ -3,11 +3,11 @@ package nl.averageflow.springwarehouse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class JoeswarehouseApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class JoeswarehouseApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
