85545ee614f770ebd4d068beeb89c5e6ce75bde0
averageflow <jjbigorra@gmail.com>
2021-12-20 21:01:03 +0100

    add category controller

---
+++ app/src/test/java/nl/averageflow/springwarehouse/controllers/CategoryControllerTest.java
@@ -0,0 +1,19 @@
+package nl.averageflow.springwarehouse.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class CategoryControllerTest {
+
+    @Autowired
+    private CategoryController controller;
+
+    @Test
+    public void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
+}
