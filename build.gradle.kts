6d4dccc3cfcac67efbe01c273417992ea1b44e12
Josep Bigorra <josep.bigorra@ingka.ikea.com>
2022-01-24 12:50:43 +0100

    Improve approach to versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,30 @@
+val baseDockerImage: String = "eclipse-temurin:17.0.1_12-jre-alpine"
+val projectVersion: String = "1.0.0"
+val projectGroup: String = "nl.averageflow.springwarehouse"
+
+object Versions {
+    const val checkstyle = "8.42"
+    const val jacoco = "0.8.7"
+    const val junit5 = "5.8.2"
+    const val springBootVersion = "2.6.3"
+    const val jibVersion = "3.2.0"
+    const val jaxbVersion = "2.3.2"
+    const val liquibaseVersion = "4.7.1"
+    const val springDocOpenAPIVersion = "1.6.3"
+    const val postgresqlVersion = "42.3.1"
+    const val h2Version = "1.4.200"
+    const val springSecurityTestVersion = "5.6.1"
+    const val javaFakerVersion = "1.0.2"
+}
+
 plugins {
     idea
     `java-library`
     jacoco
     eclipse
     // checkstyle
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("org.springframework.boot") version ("2.6.2")
-    id("com.google.cloud.tools.jib") version "3.1.4"
+    id("org.springframework.boot") version ("2.6.3")
+    id("com.google.cloud.tools.jib") version ("3.2.0")
 }
 
 buildscript {
@@ -18,31 +36,6 @@ buildscript {
 }
 
 defaultTasks("clean", "build", "jacocoTestCoverageVerification")
-
-val baseDockerImage: String = "eclipse-temurin:17.0.1_12-jre-alpine"
-val projectVersion: String = "1.0.0"
-val projectGroup: String = "nl.averageflow.springwarehouse"
-
-object Versions {
-    const val atpSchema = "0.0.2-RC10"
-    const val checkstyle = "8.42"
-    const val hamcrest = "2.2"
-    const val mockito = "3.12.4"
-    const val jacoco = "0.8.7"
-    const val junit5 = "5.7.2"
-    const val logback = "1.2.3"
-    const val hdrHistogram = "2.1.12"
-    const val protoc = "3.19.1"
-    const val javaxAnnotation = "1.3.2"
-    const val grpc = "1.33.0"
-    const val springCloudGcp = "2.0.4"
-    const val springBootVersion = "2.6.2"
-    const val testcontainers = "1.16.0"
-    const val jackson = "2.12.2"
-    const val lombok = "1.18.12"
-    const val springIntegrationCore = "5.5.3"
-    const val jodaTime = "2.10.10"
-}
 
 tasks {
     named("jar") {
@@ -76,9 +69,9 @@ allprojects {
     version = projectVersion
 
     repositories {
-        maven("https://plugins.gradle.org/m2/")
         mavenCentral()
-        mavenLocal()
+        gradlePluginPortal()
+        maven("https://plugins.gradle.org/m2/")
     }
 }
 
@@ -86,23 +79,8 @@ subprojects {
     apply(plugin = "idea")
     apply(plugin = "java-library")
     apply(plugin = "eclipse")
-    // apply(plugin = "checkstyle")
     apply(plugin = "jacoco")
-    apply(plugin = "io.spring.dependency-management")
-
-    configure<io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension> {
-        imports {
-            mavenBom("org.springframework.boot:spring-boot-starter-parent:${Versions.springBootVersion}")
-        }
-    }
-
-    dependencies {
-        testImplementation("org.hamcrest", "hamcrest", Versions.hamcrest)
-        testImplementation("org.mockito", "mockito-junit-jupiter", Versions.mockito)
-        testImplementation("org.junit.jupiter", "junit-jupiter-api", Versions.junit5)
-        testImplementation("org.junit.jupiter", "junit-jupiter-params", Versions.junit5)
-        testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", Versions.junit5)
-    }
+    // apply(plugin = "checkstyle")
 
 //    checkstyle {
 //        sourceSets = singletonList(project.sourceSets.main.get())
@@ -136,29 +114,6 @@ subprojects {
     }
 
     tasks {
-        jar {
-            enabled = true
-        }
-
-        test {
-            useJUnitPlatform()
-            testLogging {
-                events("passed", "skipped", "failed")
-            }
-            testLogging {
-                showStandardStreams = true
-                exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-            }
-            systemProperties(
-                mapOf(
-                    // ADD COMMON SYSTEM PROPERTIES FOR TESTS HERE
-                    "exampleProperty" to "exampleValue"
-                )
-            )
-            reports.html.required.set(false) // Disable individual test reports
-            finalizedBy("jacocoTestReport")
-        }
-
         jacocoTestReport {
             reports {
                 xml.required.set(true)
@@ -201,40 +156,7 @@ subprojects {
         check {
             finalizedBy(jacocoTestCoverageVerification)
         }
-    }
-}
 
-project(":app") {
-    apply(plugin = "org.springframework.boot")
-    apply(plugin = "com.google.cloud.tools.jib")
-
-    dependencies {
-        implementation("org.glassfish.jaxb:jaxb-runtime:2.3.2")
-        implementation("org.liquibase:liquibase-core:4.6.2")
-        implementation("org.springdoc:springdoc-openapi-ui:1.6.3")
-        implementation("org.springframework.boot", "spring-boot-starter-data-jpa")
-        implementation("org.springframework.boot", "spring-boot-starter-json")
-        implementation("org.springframework.boot", "spring-boot-starter-web")
-        implementation("org.springframework.boot", "spring-boot-starter-validation")
-        implementation("org.springframework.boot", "spring-boot-starter-webflux")
-        implementation("org.springframework.boot", "spring-boot-starter-security")
-        implementation("org.springframework.boot", "spring-boot-starter-actuator")
-        implementation("org.springframework.boot:spring-boot-starter-mail")
-
-        runtimeOnly("org.springframework.boot:spring-boot-devtools:2.6.2")
-        runtimeOnly("org.postgresql:postgresql:42.3.1")
-
-        testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.2")
-        testImplementation("junit:junit:4.13.2")
-        testImplementation("com.h2database:h2:1.4.200")
-        testImplementation("org.springframework.security:spring-security-test:5.6.1")
-        testImplementation("com.github.javafaker:javafaker:1.0.2")
-    }
-
-    tasks {
-        bootJar {
-            enabled = true
-        }
         test {
             useJUnitPlatform()
             testLogging {
@@ -253,6 +175,43 @@ project(":app") {
             reports.html.required.set(false) // Disable individual test reports
 
             finalizedBy("jacocoTestReport", "jacocoTestCoverageVerification")
+        }
+    }
+}
+
+project(":app") {
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "com.google.cloud.tools.jib")
+
+    dependencies {
+        implementation("org.glassfish.jaxb","jaxb-runtime", Versions.jaxbVersion)
+        implementation("org.liquibase","liquibase-core", Versions.liquibaseVersion)
+        implementation("org.springdoc","springdoc-openapi-ui", Versions.springDocOpenAPIVersion)
+        implementation("org.springframework.boot", "spring-boot-starter-data-jpa", Versions.springBootVersion)
+        implementation("org.springframework.boot", "spring-boot-starter-json", Versions.springBootVersion)
+        implementation("org.springframework.boot", "spring-boot-starter-web", Versions.springBootVersion)
+        implementation("org.springframework.boot", "spring-boot-starter-validation", Versions.springBootVersion)
+        implementation("org.springframework.boot", "spring-boot-starter-webflux", Versions.springBootVersion)
+        implementation("org.springframework.boot", "spring-boot-starter-security", Versions.springBootVersion)
+        implementation("org.springframework.boot", "spring-boot-starter-actuator", Versions.springBootVersion)
+        implementation("org.springframework.boot","spring-boot-starter-mail", Versions.springBootVersion)
+
+        runtimeOnly("org.springframework.boot", "spring-boot-devtools", Versions.springBootVersion)
+        runtimeOnly("org.postgresql","postgresql", Versions.postgresqlVersion)
+
+        testImplementation("org.springframework.boot", "spring-boot-starter-test", Versions.springBootVersion)
+        testImplementation("com.h2database","h2", Versions.h2Version)
+        testImplementation("org.springframework.security","spring-security-test", Versions.springSecurityTestVersion)
+        testImplementation("com.github.javafaker","javafaker", Versions.javaFakerVersion)
+    }
+
+    tasks {
+        jar {
+            enabled = true
+        }
+
+        bootJar {
+            enabled = true
         }
     }
 
